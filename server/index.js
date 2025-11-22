85834cb5234ab342284eee0e33aa8c4a49697678
Viktoriia Melnyk <melnychka1993@gmail.com>
2025-11-22 22:59:16 +0000

    changed client url on server

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server, {
 	cors: {
-		origin: 'http://localhost:3000',
+		origin: CLIENT_HOST,
 		methods: ['GET', 'POST'],
 	},
 });
